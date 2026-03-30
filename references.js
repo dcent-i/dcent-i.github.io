0b2cb55f55811856580bbc718854fd0ecf2a4f87
duochanatharvard <chanduo.as@gmail.com>
2026-03-30 10:45:06 +0100

    annual update 2025 data final

--- references.js
+++ references.js
@@ -1,11 +1,12 @@
 const refs = {
 
-    chan2025dcenti: `Chan D., Chan, S. C., Siddons, J. T., Cable, A., 
-        Cornes, R. C., Kent, E. C., Gebbie, G., & Huybers, P. (2026). 
-        DCENT-I: A Globally Infilled Extension of the Dynamically Consistent ENsemble of Temperature Dataset.
-        <b style="font-weight: 550;"><i> Geoscience Data Journal</i></b>.
-        <!-- <a href="https://doi.org/10.1038/s41597-024-03742-x" class="link-light">link</a>,
-        <a href="papers/Chan_et_al_2024_SD.pdf">pdf</a>. -->`,
+    chan2025dcenti: `Chan D., Chan, S. C., Siddons, J. T., Cable, A., Faulkner, A., Cornes, R. C., ... & Huybers, P. (2026).
+        DCENT‐I: A Globally Infilled Extension of the Dynamically Consistent ENsemble of Temperature Dataset.
+        <b style="font-weight: 550;"><i>Geoscience Data Journal</i></b>, 13(2), e70054.
+        <a href="https://doi.org/10.1002/gdj3.70054" class="link-light">link</a>,
+        <a href="papers/Chan_et_al_2026_DCENT-I.pdf" class="link-light">pdf</a>,
+        <a href="https://doi.org/10.7910/DVN/ROG38Q" class="link-light">data</a>,
+        <a href="https://github.com/dcent-i/DCENT-infilling" class="link-light">code</a>.`,
 
     chan2024sdata: `Chan D., Gebbie G., Huybers P., & Kent E. (2024).
     A Dynamically Consistent ENsemble of Temperature at the Earth surface since 1850 from the DCENT dataset.
